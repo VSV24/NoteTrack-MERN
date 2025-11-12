ff903e7d8dbb826cd4366450238ac39323c4ae81
VSV24 <134763629+VSV24@users.noreply.github.com>
2025-11-12 17:36:15 +0530

    created a mongodb model with notes schema

---
+++ backend/src/models/Note.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const noteSchema = new mongoose.Schema(
+  {
+    title:{
+      type: String,
+      required: true
+    },
+    content:{
+      type:String,
+      required:true
+    }
+  },{timestamps:true}//createdAt updatedAll
+);
+
+const Note = mongoose.model('Note',noteSchema)
+
+export default Note
